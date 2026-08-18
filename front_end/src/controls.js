52f9ab6ec9d9f27edc03f95c2f7298e6e2df6907
Amar Cheema <amr.cheema@yahoo.com>
2026-08-18 23:57:38 +0100

    Final Checks - slightly adjusted speed.

--- front_end/src/controls.js
+++ front_end/src/controls.js
@@ -251,7 +251,7 @@ export function animateMove(
   let finalAngle = moveInfo.angle;
 
   // velocity of animation
-  const speed = 0.14;
+  const speed = 0.12;
   let direction = 1;
   // set for anticlockwise rotations
   if (finalAngle < 0) {
